ed4e26db7512ad1ef4dcde1dca229d2dc95438ae
Cyrille TCHINDE <tchindec111@gmail.com>
2025-04-21 16:22:34 +0200

    erge avec origin main

--- routes/User.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userCtr = require('../controllers/User.controller');
-const auth = require('../middlewares/auth')
-
-router.post('/signup', userCtr.signup);
-router.post('/login', userCtr.login);
-router.get('/getUser',auth.protect, userCtr.getUser);
-
-module.exports = router;
